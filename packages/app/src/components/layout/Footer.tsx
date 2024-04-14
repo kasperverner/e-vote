d04471c04f11f4c952210055f66e5c61fbca8c33
Frederik Gustav <120267919+frederikgustav@users.noreply.github.com>
2024-04-14 20:27:02 +0200

    added empty footer

--- packages/app/src/components/layout/Footer.tsx
+++ packages/app/src/components/layout/Footer.tsx
@@ -1,5 +1,16 @@
+import Container from "./Container";
+
 const Footer = () => {
-  return <div className="">Footer?</div>;
-};
+  return (
+  <div className="bg-slate-300 py-4">
+    <Container>
+      <div className="flex justify-between items-center">
+        <div className="flex items-center">
+          
+        </div>
+      </div>
+    </Container>
+  </div>)
+  };
 
 export default Footer;
