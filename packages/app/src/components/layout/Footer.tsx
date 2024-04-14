297864a760531a6a341dd0fba807c4aed0e9b3ed
Frederik Gustav <120267919+frederikgustav@users.noreply.github.com>
2024-04-14 21:15:47 +0200

    added text and icons

--- packages/app/src/components/layout/Footer.tsx
+++ packages/app/src/components/layout/Footer.tsx
@@ -1,16 +1,34 @@
 import Container from "./Container";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faLinkedin } from "@fortawesome/free-brands-svg-icons/faLinkedin";
+import { faTwitterSquare } from "@fortawesome/free-brands-svg-icons/faTwitterSquare";
+import { faFacebookSquare } from "@fortawesome/free-brands-svg-icons/faFacebookSquare";
 
 const Footer = () => {
   return (
-  <div className="bg-slate-300 py-4">
-    <Container>
-      <div className="flex justify-between items-center">
-        <div className="flex items-center">
-          
+    <div className="bg-slate-300 py-4">
+      <Container>
+        <div className="flex justify-between items-center">
+          <div className="flex items-center space-x-6">
+            <p>© eDemokrati ApS</p>
+            <p>All rights reserved</p>
+            <p>CVR 32887848</p>
+          </div>
+          <div className="flex items-center">
+            <a href="https://www.linkedin.com/" target="_blank" rel="noopener noreferrer" className="font-extrabold hover:underline mr-1">
+              <FontAwesomeIcon icon={faLinkedin} className="size-8" />
+            </a>
+            <a href="https://twitter.com/" target="_blank" rel="noopener noreferrer" className="font-extrabold hover:underline mr-1">
+              <FontAwesomeIcon icon={faTwitterSquare} className="size-8" />
+            </a>
+            <a href="https://www.facebook.com/" target="_blank" rel="noopener noreferrer" className="font-extrabold hover:underline">
+              <FontAwesomeIcon icon={faFacebookSquare} className="size-8" />
+            </a>
+          </div>
         </div>
-      </div>
-    </Container>
-  </div>)
-  };
+      </Container>
+    </div>
+  );
+};
 
 export default Footer;
