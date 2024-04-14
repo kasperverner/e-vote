4050184226bae127fa6b81b04fc29a3088f851ad
Kasper Verner <mail@kasperverner.com>
2024-04-14 14:17:50 +0200

    rename step 2

--- packages/app/src/main.tsx
+++ packages/app/src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './_App'
+import App from './App'
 import './assets/styles/main.css'
 
 const rootElement = document.getElementById("root")!;
