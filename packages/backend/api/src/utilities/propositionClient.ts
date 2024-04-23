5109e37d9406654a6968ba2b2c80141c748b639a
Kasper Verner <mail@kasperverner.com>
2024-04-23 11:14:50 +0200

    implemented proof validation and results tally for the backend services

--- packages/backend/api/src/utilities/propositionClient.ts
+++ packages/backend/api/src/utilities/propositionClient.ts
@@ -1,3 +1,6 @@
+import { ElectionResultsResponse } from "../types/ElectionResultsResponse";
+import { ValidationResponse } from "../types/ValidationResponse";
+
 export const generatePropositionProof = async (propositionId: string) => {
   const url = `${process.env.PROPOSITION_SERVICE_URL}/proofs/${propositionId}`;
   const response = await fetch(url);
@@ -5,3 +8,19 @@ export const generatePropositionProof = async (propositionId: string) => {
 
   return proof;
 };
+
+export const validatePropositionProofsForElection = async (electionId: string): Promise<ValidationResponse> => {
+  const url = `${process.env.PROPOSITION_SERVICE_URL}/proofs/validate/${electionId}`;
+  const response = await fetch(url);
+  const result = await response.json();
+
+  return result as ValidationResponse;
+};
+
+export const getResultForElection = async (electionId: string) : Promise<ElectionResultsResponse> => {
+  const url = `${process.env.PROPOSITION_SERVICE_URL}/results/${electionId}`;
+  const response = await fetch(url);
+  const results = await response.json();
+
+  return results;
+}
--- packages/backend/proposition-service/src/index.ts
+++ packages/backend/proposition-service/src/index.ts
@@ -2,6 +2,7 @@ import * as dotenv from "dotenv";
 import express from "express";
 import cors from "cors";
 import proofRoutes from "./routes/proofs";
+import resultRoutes from "./routes/results";
 
 dotenv.config();
 
@@ -13,6 +14,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/proofs", proofRoutes);
+app.use("/results", resultRoutes);
 
 app.listen(PORT, async () => {
   console.log(`Proposition service started on http://localhost:${PORT}`);
