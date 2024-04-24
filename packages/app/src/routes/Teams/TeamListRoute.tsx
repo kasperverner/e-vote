dda8165338d97c9a6cdc7765328411049a26b615
Frederik Gustav <120267919+frederikgustav@users.noreply.github.com>
2024-04-24 10:44:43 +0200

    enhanced teams page layout

--- packages/app/src/routes/Teams/TeamListRoute.tsx
+++ packages/app/src/routes/Teams/TeamListRoute.tsx
@@ -4,6 +4,7 @@ import useTeams from "../../hooks/useTeams";
 import { useAuth } from "@clerk/clerk-react";
 import Container from "../../components/layout/Container";
 import { Link } from "@tanstack/react-router";
+import Button from "../../components/form/button";
 
 const TeamListRoute = createRoute({
   getParentRoute: () => TeamIndexRoute,
@@ -14,8 +15,6 @@ const TeamListRoute = createRoute({
 function TeamListPage() {
   useAuth();
   const { data } = useTeams();
-  // print all teams
-  console.log(data);
 
   if (!data) {
     return <Container>Loading...</Container>;
@@ -26,14 +25,13 @@ function TeamListPage() {
       <h1 className="text-3xl font-bold mb-4">Teams</h1>
       <ul>
         {data?.map((team) => (
-            <li key={team.id} className="bg-white p-4 rounded-lg shadow mb-4 hover:bg-slate-200">
-              <Link to={`/Teams/${team.id}`} className="text-xl font-bold">
-                <Container>
-                  <h2 className="text-xl font-bold">{team.name}</h2>
-                  <p className="text-sm text-gray-500">Created: {new Date(team.created_at).toLocaleDateString()}</p>
-                </Container>
-              </Link>
-            </li>
+          <li key={team.id} className="bg-white p-4 rounded-lg shadow mb-4 hover:bg-slate-200 flex items-center justify-between">
+            <div>
+              <h2 className="text-xl font-bold">{team.name}</h2>
+              <p className="text-sm text-gray-500">{team._count.members} members</p>
+            </div>
+            <Button to={`/Teams/${team.id}`}>See details</Button>
+          </li>
         ))}
       </ul>
     </Container>
