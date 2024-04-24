6863f165b98f2f36de2d4228d26e1debcf76c82f
Frederik Gustav <120267919+frederikgustav@users.noreply.github.com>
2024-04-24 09:39:54 +0200

    added layout for teams

--- packages/app/src/routes/Teams/TeamListRoute.tsx
+++ packages/app/src/routes/Teams/TeamListRoute.tsx
@@ -1,5 +1,9 @@
 import { createRoute } from "@tanstack/react-router";
 import TeamIndexRoute from "./TeamIndexRoute";
+import useTeams from "../../hooks/useTeams";
+import { useAuth } from "@clerk/clerk-react";
+import Container from "../../components/layout/Container";
+import { Link } from "@tanstack/react-router";
 
 const TeamListRoute = createRoute({
   getParentRoute: () => TeamIndexRoute,
@@ -8,12 +12,31 @@ const TeamListRoute = createRoute({
 });
 
 function TeamListPage() {
+  useAuth();
+  const { data } = useTeams();
+  // print all teams
+  console.log(data);
+
+  if (!data) {
+    return <Container>Loading...</Container>;
+  }
+
   return (
-    <div className="">
-      <h3>TeamListPage</h3>
-
-
-    </div>
+    <Container>
+      <h1 className="text-3xl font-bold mb-4">Teams</h1>
+      <ul>
+        {data?.map((team) => (
+            <li key={team.id} className="bg-white p-4 rounded-lg shadow mb-4 hover:bg-slate-200">
+              <Link to={`/Teams/${team.id}`} className="text-xl font-bold">
+                <Container>
+                  <h2 className="text-xl font-bold">{team.name}</h2>
+                  <p className="text-sm text-gray-500">Created: {new Date(team.created_at).toLocaleDateString()}</p>
+                </Container>
+              </Link>
+            </li>
+        ))}
+      </ul>
+    </Container>
   );
 }
 
