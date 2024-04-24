03ca2db5cc4f0f86ea1c610995f8631c11f798d8
Frederik Gustav <120267919+frederikgustav@users.noreply.github.com>
2024-04-24 11:24:06 +0200

    Update NotFoundRoute.tsx

--- packages/app/src/routes/NotFoundRoute.tsx
+++ packages/app/src/routes/NotFoundRoute.tsx
@@ -11,9 +11,11 @@ function NotFoundPage() {
   const url = document.location.pathname;
 
   return (
-    <div className="">
-      <h3>404 - NotFoundPage: {url}</h3>
-    </div>
+  <div className="flex flex-col items-center justify-center h-96">
+    <h1 className="text-4xl font-bold mb-4 text-red-500">404 - Not Found</h1>
+    <p className="text-lg text-gray-700 mb-4">The page at "{url}" could not be found.</p>
+    <a href="/" className="text-blue-500 hover:underline">Go back to homepage</a>
+  </div>
   );
 }
 
