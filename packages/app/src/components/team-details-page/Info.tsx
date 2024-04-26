72a97e6c7fc4a3b40e4844c765109b7b04f691e3
Frederik Gustav <120267919+frederikgustav@users.noreply.github.com>
2024-04-26 13:02:53 +0200

    initial details structure

---
+++ packages/app/src/components/team-details-page/Info.tsx
@@ -0,0 +1,17 @@
+const Info = ({team}) => {
+    return (
+        <div className="flex flex-col space-y-4 w-1/3">
+            {team ? (
+                <div>
+                    <h1 className="text-3xl font-bold text-gray-800">{team.name}</h1>
+                    <p className="text-xl text-gray-600">Total Members: {team._count.members}</p>
+                    <p className="text-l text-gray-600">Created At: {new Date(team.created_at).toLocaleDateString()}</p>
+                </div>
+            ) : (
+                <p className="text-xl text-gray-600">Loading...</p>
+            )}
+        </div>
+    )
+}
+
+export default Info;
--- packages/app/src/routes/Teams/TeamDetailsRoute.tsx
+++ packages/app/src/routes/Teams/TeamDetailsRoute.tsx
@@ -1,5 +1,14 @@
 import { createRoute } from "@tanstack/react-router";
 import TeamIndexRoute from "./TeamIndexRoute";
+import useTeam from "../../hooks/useTeam";
+import useTeamMembers from "../../hooks/useTeamMembers";
+import useInvitations from "../../hooks/useInvitations";
+
+import Button from "../../components/form/button";
+import Info from "../../components/team-details-page/Info";
+import TabPage from "../../components/team-details-page/TabPage";
+import Members from "../../components/team-details-page/Members";
+import Invitations from "../../components/team-details-page/Invitations";
 
 const TeamDetailsRoute = createRoute({
   getParentRoute: () => TeamIndexRoute,
@@ -9,10 +18,30 @@ const TeamDetailsRoute = createRoute({
 
 function TeamDetailsPage() {
   const { team_slug } = TeamDetailsRoute.useParams();
+  const { data: team } = useTeam(team_slug);
+  const { data: memberInfo } = useTeamMembers(team_slug);
+  const { data: invitations } = useInvitations(team_slug);
 
   return (
-    <div className="">
-      <h3>TeamDetailsPage {team_slug}!</h3>
+    <div className="flex flex-col space-y-6">
+      <div className="bg-gray-100 p-6 rounded-lg shadow-md flex justify-between">
+        <div>
+          <Info team={team} />
+          <Button className="mt-3 bg-orange-500 w-2/3">Leave team</Button>
+          {team?.isAdmin && <Button className="mt-2 bg-yellow-500 w-2/3">Rename team</Button>}
+          {team?.isAdmin && <Button className="mt-2 bg-red-500 w-2/3">Delete team</Button>}
+        </div>
+
+        <div id="target" className="flex flex-col space-y-4 w-2/3">
+          <TabPage components={[
+            <Members memberInfo={memberInfo} />,
+            <Invitations invitations={invitations} />
+          ]} names={[
+            "Members",
+            "Invitations",
+          ]} />
+        </div>
+      </div>
     </div>
   );
 }
