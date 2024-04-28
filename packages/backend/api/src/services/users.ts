90cca66ab9e16a8d3bcff52fb8a80c1484868d01
Frederik Gustav <120267919+frederikgustav@users.noreply.github.com>
2024-04-28 16:07:00 +0200

    fixed endpoint for getting current user

--- packages/backend/api/src/services/users.ts
+++ packages/backend/api/src/services/users.ts
@@ -15,20 +15,9 @@ export const getAuthenticatedUser: RequestHandler = async (req, res) => {
   // Check if user exists in the database
   let user = await db.users.findUnique({
     where: {
-      principal_id: user_id,
+      id: user_id,
     },
   });
-
-  // If user does not exist, create a new user in the database
-  if (!user) {
-    user = await db.users.create({
-      data: {
-        principal_id: user_id,
-        name,
-        email,
-      },
-    });
-  }
 
   return res.status(200).json({ user });
 };
