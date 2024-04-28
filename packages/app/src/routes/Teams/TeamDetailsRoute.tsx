2496361a97d52e406a6d6da250d43beb8ede13e5
Frederik Gustav <120267919+frederikgustav@users.noreply.github.com>
2024-04-28 16:12:42 +0200

    Update TeamDetailsRoute.tsx

--- packages/app/src/routes/Teams/TeamDetailsRoute.tsx
+++ packages/app/src/routes/Teams/TeamDetailsRoute.tsx
@@ -2,13 +2,11 @@ import { createRoute } from "@tanstack/react-router";
 import TeamIndexRoute from "./TeamIndexRoute";
 import useTeam from "../../hooks/useTeam";
 import useTeamMembers from "../../hooks/useTeamMembers";
-import useInvitations from "../../hooks/useInvitations";
+import useCurrentUser from "../../hooks/useCurrentUser";
 
 import Button from "../../components/form/button";
 import Info from "../../components/team-details-page/Info";
-import TabPage from "../../components/team-details-page/TabPage";
-import Members from "../../components/team-details-page/Members";
-import Invitations from "../../components/team-details-page/Invitations";
+import AdminView from "../../components/team-details-page/AdminView";
 
 const TeamDetailsRoute = createRoute({
   getParentRoute: () => TeamIndexRoute,
@@ -20,30 +18,50 @@ function TeamDetailsPage() {
   const { team_slug } = TeamDetailsRoute.useParams();
   const { data: team } = useTeam(team_slug);
   const { data: memberInfo } = useTeamMembers(team_slug);
-  const { data: invitations } = useInvitations(team_slug);
+  const { data: currentUser } = useCurrentUser();
+  console.log(currentUser);
+  const userIsAdmin = memberInfo?.find((member) => member.user_id === currentUser?.id)?.isAdmin;
+
+  // if anything is loading, return a loading state
+  if (!team || !memberInfo || !currentUser) {
+    return <div>Loading...</div>;
+  }
 
   return (
     <div className="flex flex-col space-y-6">
       <div className="bg-gray-100 p-6 rounded-lg shadow-md flex justify-between">
-        <div>
+        <div className="flex flex-col space-y-4 w-1/2">
           <Info team={team} />
-          <Button className="mt-3 bg-orange-500 w-2/3">Leave team</Button>
-          {team?.isAdmin && <Button className="mt-2 bg-yellow-500 w-2/3">Rename team</Button>}
-          {team?.isAdmin && <Button className="mt-2 bg-red-500 w-2/3">Delete team</Button>}
+          {userIsAdmin && <Button to={`/Teams/${team_slug}/admin`} className="mt-1 bg-green-500 w-1/2" >Admin panel</Button>}
+          <Button className="mt-3 bg-orange-500 w-1/2" onClick={() => {handleLeaveTeam(currentUser?.id, memberInfo)}}>Leave team</Button>
         </div>
 
-        <div id="target" className="flex flex-col space-y-4 w-2/3">
-          <TabPage components={[
-            <Members memberInfo={memberInfo} />,
-            <Invitations invitations={invitations} />
-          ]} names={[
-            "Members",
-            "Invitations",
-          ]} />
+        <div className="flex flex-col space-y-4 w-1/2">
+          <AdminView memberInfo={memberInfo?.filter((member) => {return member.isAdmin})} />
         </div>
       </div>
     </div>
   );
+}
+
+function handleLeaveTeam(user_id, members) {
+  const isAdmin = members.some((member) => member.user_id === user_id && member.isAdmin);
+  const adminCount = members.filter((member) => member.isAdmin).length;
+  console.log(isAdmin, adminCount);
+
+  if (isAdmin && adminCount === 1) {
+    console.log("You are the only admin");
+    // Delete the team
+    // TODO: Implement team deletion logic
+  } else if (isAdmin) {
+    console.log("You are an admin but not the only one");
+    // Demote itself and leave
+    // TODO: Implement demotion and leave logic
+  } else {
+    console.log("You are not an admin");
+    // Just leave the team
+    // TODO: Implement leave logic
+  }
 }
 
 export default TeamDetailsRoute;
