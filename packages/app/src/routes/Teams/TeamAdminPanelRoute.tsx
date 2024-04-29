2ac6aad2a4cc9e044b6072e3d85175379a5dad39
Frederik Gustav <120267919+frederikgustav@users.noreply.github.com>
2024-04-29 09:18:26 +0200

    invitation functionality

--- packages/app/src/routes/Teams/TeamAdminPanelRoute.tsx
+++ packages/app/src/routes/Teams/TeamAdminPanelRoute.tsx
@@ -4,6 +4,7 @@ import { useAuth } from "@clerk/clerk-react";
 import useElections from "../../hooks/useElections";
 import useTeamMembers from "../../hooks/useTeamMembers";
 import useCurrentUser from "../../hooks/useCurrentUser";
+import useInvitations from "../../hooks/useInvitations";
 
 import Button from "../../components/form/button";
 import { Link } from "@tanstack/react-router";
@@ -16,16 +17,14 @@ const TeamAdminPanelRoute = createRoute({
 
 function TeamAdminPanel() {
     const team_slug = TeamAdminPanelRoute.useParams().team_slug;
-    console.log(team_slug);
     const { getToken } = useAuth();
     const { data: elections } = useElections(team_slug);
     const { data: members } = useTeamMembers(team_slug);
     const { data: currentUser } = useCurrentUser();
-
-    console.log(currentUser);
+    const { data: invitations } = useInvitations(team_slug);
 
     // if something is loading, return a loading state
-    if (!elections || !members) {
+    if (!elections || !members || !currentUser || !invitations) {
         return <div>Loading...</div>;
     }
 
@@ -80,7 +79,6 @@ function TeamAdminPanel() {
     }
 
     async function handleChangeUser(user_id: string, isAdmin: boolean) {
-        console.log("Promote user with id: ", user_id);
         // fetch put on /teams/:teamId/members/:memberId, add isAdmin: true
         const token = await getToken();
         fetch(`http://localhost:4000/teams/${team_slug}/members/${user_id}`, {
@@ -124,6 +122,51 @@ function TeamAdminPanel() {
             });
     }
 
+    async function handleInviteUser(email: string) {
+        // fetch post request to invite user to team (POST on /teams/:teamId/members/invitations)
+        const token = await getToken();
+        fetch(`http://localhost:4000/teams/${team_slug}/members/invitations`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                Authorization: `Bearer ${token as string}`,
+            },
+            body: JSON.stringify({
+                email: email,
+                isAdmin: false,
+            }),
+        })
+            .then((data) => {
+                console.log(data);
+                // reload page
+                window.location.reload();
+            })
+            .catch((error) => {
+                console.error("Error:", error);
+                alert("Error inviting user to team");
+            });
+    }
+
+    async function handleRevokeInvitation(invitation_id: string) {
+        // fetch delete request to revoke invitation (DELETE on /teams/:teamId/members/invitations/:invitationId)
+        const token = await getToken();
+        fetch(`http://localhost:4000/teams/${team_slug}/members/invitations/${invitation_id}`, {
+            method: "DELETE",
+            headers: {
+                Authorization: `Bearer ${token as string}`,
+            },
+        })
+            .then((data) => {
+                console.log(data);
+                // reload page
+                window.location.reload();
+            })
+            .catch((error) => {
+                console.error("Error:", error);
+                alert("Error revoking invitation");
+            });
+    }
+
     return (
         <div className="flex flex-row" style={{ height: "80vh" }}>
             <div className="flex flex-col w-1/2">
@@ -132,16 +175,21 @@ function TeamAdminPanel() {
                     <ul className="flex flex-col space-y-4">
                         {members.map((member) => (
                             <li key={member.email} className="bg-gray-100 p-4 rounded-lg shadow-md flex justify-between items-center">
-                                <div>
-                                    <h3 className="text-lg font-bold">{member.name}</h3>
-                                    <p>{member.email}</p>
+                                <div className="flex items-center">
+                                    <div>
+                                        <h3 className="text-lg font-bold">{member.name}</h3>
+                                        <p>{member.email}</p>
+                                    </div>
+                                    {member.isAdmin && (
+                                        <span className="text-sm text-green-600 font-semibold ml-4">Admin</span>
+                                    )}
                                 </div>
                                 {currentUser.user.email !== member.email && (
                                     <div className="flex space-x-2">
                                         {!member.isAdmin ? (
-                                            <Button className="bg-green-500 text-white px-4 py-2 rounded-md" onClick={() => handlePromoteUser(member.id)}>Promote</Button>
+                                            <Button className="bg-green-500 text-white px-4 py-2 rounded-md" onClick={() => handleChangeUser(member.id, true)}>Promote</Button>
                                         ) : (
-                                            <Button className="bg-yellow-500 text-white px-4 py-2 rounded-md" onClick={() => handleDemoteUser(member.id)}>Demote</Button>
+                                            <Button className="bg-yellow-500 text-white px-4 py-2 rounded-md" onClick={() => handleChangeUser(member.id, false)}>Demote</Button>
                                         )}
                                         <Button className="bg-red-500 text-white px-4 py-2 rounded-md" onClick={() => handleRemoveUser(member.id)}>Kick</Button>
                                     </div>
@@ -151,10 +199,26 @@ function TeamAdminPanel() {
                     </ul>
                 </div>
 
-                <div className="bg-white p-4 rounded-lg shadow-md mb-4 h-2/5">
+                <div className="bg-white p-4 rounded-lg shadow-md mb-4 h-2/5 overflow-y-auto">
                     <h2 className="text-xl font-bold mb-4">Invites</h2>
-                    {/* Invites section content */}
+                    <div>
+                        <div className="flex mb-4">
+                            <input type="email" placeholder="Enter email" id="inviteEmail" className="border border-gray-300 rounded-md px-2 py-1" />
+                            <Button className="ml-2 bg-green-500 text-white px-4 py-2 rounded-md" onClick={() => handleInviteUser(document.getElementById('inviteEmail').value)}>Invite</Button>
+                        </div>
+                    </div>
+                    <ul className="flex flex-col space-y-4 h-full ml-4">
+                        {invitations.map((invitation, index) => (
+                            <li key={invitation.email + index} className="bg-gray-100 p-4 rounded-lg shadow-md flex justify-between items-center">
+                                <div>
+                                    <h3 className="text-lg">{invitation.email}</h3>
+                                </div>
+                                <Button className="bg-red-500 text-white px-4 py-2 rounded-md" onClick={() => handleRevokeInvitation(invitation.id)}>Revoke</Button>
+                            </li>
+                        ))}
+                    </ul>
                 </div>
+
                 <div className="bg-white p-4 rounded-lg shadow-md h-1/5">
                     <h2 className="text-xl font-bold mb-4">Actions</h2>
                     <div>
