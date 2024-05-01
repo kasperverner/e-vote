3a3b3c63370ca99abecfd73a8dd4c1a6717c56c0
Kasper Verner <mail@kasperverner.com>
2024-05-01 14:43:18 +0200

    refactored create election

--- packages/app/src/routes/Teams/TeamElectionCreateRoute.tsx
+++ packages/app/src/routes/Teams/TeamElectionCreateRoute.tsx
@@ -1,7 +1,26 @@
-import { useState } from "react";
-import { createRoute, useNavigate } from "@tanstack/react-router";
-import { useAuth } from "@clerk/clerk-react";
+import { createRoute } from "@tanstack/react-router";
 import TeamIndexRoute from "./TeamIndexRoute";
+import useCreateElection, {
+  ElectionRequest,
+} from "../../hooks/useCreateElection";
+import { z, ZodType } from "zod";
+import { useFieldArray, useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+const ElectionSchema: ZodType<ElectionRequest> = z.object({
+  name: z.string().min(1),
+  description: z.string().optional(),
+  start_at: z.string().min(1),
+  end_at: z.string().min(1),
+  propositions: z
+    .array(
+      z.object({
+        name: z.string().min(1),
+        description: z.string().optional(),
+      })
+    )
+    .min(2),
+});
 
 const TeamElectionCreateRoute = createRoute({
   getParentRoute: () => TeamIndexRoute,
@@ -11,232 +30,99 @@ const TeamElectionCreateRoute = createRoute({
 
 function TeamElectionCreatePage() {
   const { team_id } = TeamElectionCreateRoute.useParams();
-  const { getToken } = useAuth();
-  const navigate = useNavigate();
+  const createElection = useCreateElection(team_id);
 
-  if (!team_id) {
-    return <div>Loading...</div>;
-  }
-
-  const clientFormat = (datetime) => {
-    // Ensure datetime is in the format expected by datetime-local input
-    return datetime.replace("Z", "");
-  };
-
-  const serverFormat = (datetime) => {
-    // Parse input time as local time
-    const date = new Date(datetime);
-    console.log("date", date); // This will be the local time
-
-    // Convert to UTC without altering the time value
-    const utcDateString = date.toISOString();
-    console.log("utcDate in iso", utcDateString); // This will be in UTC, maintaining the time value
-    return utcDateString;
-  };
-
-  const [formData, setFormData] = useState({
-    name: "",
-    description: "",
-    start_at_client: "",
-    end_at_client: "",
-    start_at_server: "",
-    end_at_server: "",
-    propositions: [
-      { name: "", description: "" },
-      { name: "", description: "" },
-    ],
+  const {
+    register,
+    handleSubmit,
+    control,
+    formState: { errors, isValid },
+  } = useForm<ElectionRequest>({
+    defaultValues: {
+      name: undefined,
+      description: undefined,
+      start_at: undefined,
+      end_at: undefined,
+      propositions: [{ name: undefined, description: undefined }],
+    },
+    resolver: zodResolver(ElectionSchema),
   });
 
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    if (name === "start_at_client" || name === "end_at_client") {
-      const formattedValue = clientFormat(value);
-      setFormData((prevFormData) => ({
-        ...prevFormData,
-        [name]: formattedValue,
-        [`${name.slice(0, -7)}_server`]: serverFormat(formattedValue),
-      }));
-      console.log("set it to", formattedValue);
-    } else {
-      setFormData((prevFormData) => ({
-        ...prevFormData,
-        [name]: value,
-      }));
-    }
-  };
+  const { fields, append, remove } = useFieldArray({
+    control,
+    name: "propositions",
+  });
 
-  const handlePropositionChange = (index, field, value) => {
-    const updatedPropositions = [...formData.propositions];
-    updatedPropositions[index][field] = value;
-    setFormData((prevFormData) => ({
-      ...prevFormData,
-      propositions: updatedPropositions,
-    }));
-  };
-
-  const handleAddProposition = () => {
-    setFormData({
-      ...formData,
-      propositions: [...formData.propositions, { name: "", description: "" }],
-    });
-  };
-
-  const handleRemoveProposition = (index) => {
-    const updatedPropositions = [...formData.propositions];
-    updatedPropositions.splice(index, 1);
-    setFormData({ ...formData, propositions: updatedPropositions });
-  };
-
-  // same as above but using then chain
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-
-    // check if the dates are set
-    if (!formData.start_at_server || !formData.end_at_server) {
-      alert("Please set the start and end dates");
-      return;
-    }
-
-    // check if the start date is before the end date
-    if (formData.start_at_server > formData.end_at_server) {
-      alert("The start date must be before the end date");
-      return;
-    }
-
-    // check if there are is at least two propositions
-    if (formData.propositions.length < 2) {
-      alert("Please add at least two proposition");
-      return;
-    }
-
-    // check if all names and descriptions are set (both for election and propositions)
-    if (
-      !formData.name ||
-      !formData.description ||
-      formData.propositions.some(
-        (proposition) => !proposition.name || !proposition.description
-      )
-    ) {
-      alert("Please fill in all fields");
-      return;
-    }
-
-    // disable the button to prevent double submission
-    document.getElementById("create-election-button").disabled = true;
-
-    const token = await getToken();
-    const requestOptions = {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${token}`,
-      },
-      body: JSON.stringify({
-        name: formData.name,
-        description: formData.description,
-        propositions: formData.propositions,
-        start_at: formData.start_at_server,
-        end_at: formData.end_at_server,
-      }),
-    };
-    fetch(
-      import.meta.env.VITE_API_BASE_URL + `/teams/${team_id}/elections`,
-      requestOptions
-    )
-      .then((response) => {
-        navigate({ to: `/teams/$team_id/admin`, params: { team_id } });
-      })
-      .catch((error) => {
-        console.error("Error:", error);
-      });
-    // This is not the way to do it. Make a mutation and use the useMutation isLoading state to disable the button
-    // document.getElementById("create-election-button").disabled = false;
-  };
+  const onSubmit = handleSubmit(async (data: ElectionRequest) => {
+    await createElection.mutateAsync(data);
+  });
 
   return (
-    <div className="max-w-md mx-auto">
-      <form onSubmit={handleSubmit} className="space-y-4">
+    <>
+      <h1 className="text-3xl font-bold mb-4">Create election</h1>
+      <form onSubmit={onSubmit} className="space-y-4">
         <input
-          type="text"
-          name="name"
-          value={formData.name}
-          onChange={handleChange}
+          {...register("name")}
           placeholder="Election Name"
           className="block w-full border rounded py-2 px-3 focus:outline-none focus:border-blue-500"
         />
-        <input
-          type="text"
-          name="description"
-          value={formData.description}
-          onChange={handleChange}
+        <textarea
+          rows={4}
+          {...register("description")}
           placeholder="Election Description"
           className="block w-full border rounded py-2 px-3 focus:outline-none focus:border-blue-500"
         />
         <input
           type="datetime-local"
-          name="start_at_client"
-          value={clientFormat(formData.start_at_client)}
-          onChange={handleChange}
+          {...register("start_at")}
           placeholder="Start At"
           className="block w-full border rounded py-2 px-3 focus:outline-none focus:border-blue-500"
         />
         <input
           type="datetime-local"
-          name="end_at_client"
-          value={clientFormat(formData.end_at_client)}
-          onChange={handleChange}
+          {...register("end_at")}
           placeholder="End At"
           className="block w-full border rounded py-2 px-3 focus:outline-none focus:border-blue-500"
         />
-        {formData.propositions.map((proposition, index) => (
-          <div key={index} className="space-y-2">
+        {fields.map((field, index) => (
+          <div key={field.id} className="flex flex-col gap-2">
             <input
-              type="text"
-              name={`proposition_${index}_name`}
-              value={proposition.name}
-              onChange={(e) =>
-                handlePropositionChange(index, "name", e.target.value)
-              }
+              {...register(`propositions.${index}.name`)}
               placeholder="Proposition Name"
               className="block w-full border rounded py-2 px-3 focus:outline-none focus:border-blue-500"
             />
             <input
-              type="text"
-              name={`proposition_${index}_description`}
-              value={proposition.description}
-              onChange={(e) =>
-                handlePropositionChange(index, "description", e.target.value)
-              }
+              {...register(`propositions.${index}.description`)}
               placeholder="Proposition Description"
               className="block w-full border rounded py-2 px-3 focus:outline-none focus:border-blue-500"
             />
-            {index > 1 && (
-              <button
-                type="button"
-                onClick={() => handleRemoveProposition(index)}
-                className="text-red-500 hover:text-red-700"
-              >
-                Remove
-              </button>
-            )}
+            <button
+              type="button"
+              onClick={() => remove(index)}
+              className="text-red-500 hover:text-red-700"
+            >
+              Remove
+            </button>
           </div>
         ))}
-        <button
-          type="button"
-          onClick={handleAddProposition}
-          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-        >
-          Add Proposition
-        </button>
-        <button
-          type="submit"
-          className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded"
-          id="create-election-button"
-        >
-          Create Election
-        </button>
+        <div className="flex place-content-between">
+          <button
+            type="button"
+            onClick={() => append({ name: "", description: "" })}
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+          >
+            Add Proposition
+          </button>
+          <button
+            type="submit"
+            className="bg-green-500 hover:bg-green-700 disabled:bg-green-800 text-white font-bold py-2 px-4 rounded"
+            disabled={!isValid || createElection.isPending}
+          >
+            Create Election
+          </button>
+        </div>
       </form>
-    </div>
+    </>
   );
 }
 
